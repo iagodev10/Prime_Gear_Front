aa9f4edffae810a6d20ffdbe421bc240cd1e3b82
iagodev10 <iagoborgesbarbosa7@gmail.com>
2025-11-25 19:59:55 -0300

    feat(SidebarMenu): add mobile-specific menu items and improve code formatting
    Add conditional rendering of additional menu items for mobile devices (width <= 768px) including laptops, desktops, consoles and peripherals. Also reformat code for better readability with consistent quotes and line breaks.

--- prime_gear_front/src/components/SidebarMenu/index.jsx
+++ prime_gear_front/src/components/SidebarMenu/index.jsx
@@ -1,81 +1,136 @@
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-import { Backdrop, Sidebar, SideHeader, Close, SideBody, NavList, NavItem, BestSellers } from './style';
+import {
+  Backdrop,
+  Sidebar,
+  SideHeader,
+  Close,
+  SideBody,
+  NavList,
+  NavItem,
+  BestSellers,
+} from "./style";
 
 import { FiX, FiChevronRight } from "react-icons/fi";
 
-import BestSeller from '../../assets/images/bestseller.png';
+import BestSeller from "../../assets/images/bestseller.png";
 
 const SidebarMenu = ({ isOpen, onClose }) => {
+  const handleClose = () => {
+    onClose?.();
+  };
 
-    const handleClose = () => {
-        onClose?.();
+  React.useEffect(() => {
+    const handler = (e) => {
+      if (e.key === "Escape") handleClose();
     };
+    if (isOpen) {
+      window.addEventListener("keydown", handler);
+    }
+    return () => window.removeEventListener("keydown", handler);
+  }, [isOpen, onClose]);
 
-    React.useEffect(() => {
-        const handler = (e) => {
-            if (e.key === 'Escape') handleClose();
-        };
-        if (isOpen) {
-            window.addEventListener('keydown', handler);
-        }
-        return () => window.removeEventListener('keydown', handler);
-    }, [isOpen, onClose]);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  useEffect(() => {
+    const onResize = () => setIsMobile(window.innerWidth <= 768);
+    window.addEventListener("resize", onResize);
+    return () => window.removeEventListener("resize", onResize);
+  }, []);
 
-    return (
-        <>
-            <Backdrop $isOpen={isOpen} onClick={handleClose} />
+  return (
+    <>
+      <Backdrop $isOpen={isOpen} onClick={handleClose} />
 
-            <Sidebar $isOpen={isOpen}>
-                <SideHeader>
-                    <Close onClick={handleClose}>
-                        <FiX size={24} />
-                    </Close>
-                </SideHeader>
+      <Sidebar $isOpen={isOpen}>
+        <SideHeader>
+          <Close onClick={handleClose}>
+            <FiX size={24} />
+          </Close>
+        </SideHeader>
 
-                <SideBody>
-                    <NavList>
-                        <NavItem>
-                            <Link to="/institucional" onClick={handleClose}>Sobre</Link>
-                        </NavItem>
+        <SideBody>
+          <NavList>
+            <NavItem>
+              <Link to="/institucional" onClick={handleClose}>
+                Sobre
+              </Link>
+            </NavItem>
 
-                        <NavItem>
-                            <Link to="/loja" onClick={handleClose}>Nossas Lojas</Link>
-                            <FiChevronRight size={20} />
-                        </NavItem>
+            <NavItem>
+              <Link to="/loja" onClick={handleClose}>
+                Nossas Lojas
+              </Link>
+              <FiChevronRight size={20} />
+            </NavItem>
 
-                        <NavItem>
-                            <Link to="/fale-conosco" onClick={handleClose}>Fale Conosco</Link>
-                            <FiChevronRight size={20} />
-                        </NavItem>
+            <NavItem>
+              <Link to="/fale-conosco" onClick={handleClose}>
+                Fale Conosco
+              </Link>
+              <FiChevronRight size={20} />
+            </NavItem>
 
-                        <hr />
+            <hr />
 
-                        <NavItem>
-                            <Link to="/loja" onClick={handleClose}>Categorias</Link>
-                            <FiChevronRight size={20} />
-                        </NavItem>
+            <NavItem>
+              <Link to="/loja" onClick={handleClose}>
+                Categorias
+              </Link>
+              <FiChevronRight size={20} />
+            </NavItem>
 
-                        <NavItem>
-                            <Link to="/" onClick={handleClose}>FAQs</Link>
-                        </NavItem>
+            <NavItem>
+              <Link to="/" onClick={handleClose}>
+                FAQs
+              </Link>
+            </NavItem>
 
-                    </NavList>
+            {isMobile && (
+              <>
+                <hr />
+                <NavItem>
+                  <Link to="/laptops" onClick={handleClose}>
+                    Laptops
+                  </Link>
+                  <FiChevronRight size={20} />
+                </NavItem>
+                <NavItem>
+                  <Link to="/desktops" onClick={handleClose}>
+                    Desktops
+                  </Link>
+                  <FiChevronRight size={20} />
+                </NavItem>
+                <NavItem>
+                  <Link to="/consoles" onClick={handleClose}>
+                    Consoles
+                  </Link>
+                  <FiChevronRight size={20} />
+                </NavItem>
+                <NavItem>
+                  <Link to="/perifericos" onClick={handleClose}>
+                    Periféricos
+                  </Link>
+                  <FiChevronRight size={20} />
+                </NavItem>
+              </>
+            )}
+          </NavList>
 
-                    <Link to="/loja" onClick={handleClose} style={{ display: 'block', textDecoration: 'none' }}>
-                        <BestSellers>
-                            <img src={BestSeller} alt="Bestsellers" />
-                            <h3>Produtos em Destaque</h3>
-                        </BestSellers>
-                    </Link>
-
-                </SideBody>
-
-            </Sidebar>
-        </>
-    );
-}
+          <Link
+            to="/loja"
+            onClick={handleClose}
+            style={{ display: "block", textDecoration: "none" }}
+          >
+            <BestSellers>
+              <img src={BestSeller} alt="Bestsellers" />
+              <h3>Produtos em Destaque</h3>
+            </BestSellers>
+          </Link>
+        </SideBody>
+      </Sidebar>
+    </>
+  );
+};
 
 export default SidebarMenu;
